159b934e680e17c56ea40c3a4d0263bdc4119add
Giulia Munis <grippgiulia@gmail.com>
2025-12-13 12:52:47 -0300

    criação dos index
    Criei os index nas tabelas para melhor a performace do banco de dados

---
+++ Entregável 2/index.sql
@@ -0,0 +1,6 @@
+CREATE INDEX index_constructor_name ON constructor_db (name); 
+CREATE INDEX index_drivers_nationality ON drivers_db (nationality);
+CREATE INDEX index_drivers_name ON drivers_db(name);
+CREATE INDEX index_circuits_name ON circuits_db(name); 
+CREATE INDEX index_season_year ON seasons_db(year); 
+CREATE INDEX index_status_tipo ON status_db(status); 
